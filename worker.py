a776bab8adf3fb84d2a8d8c08115ecf0f76ba98b
Erik Khalimov <biomaks@gmail.com>
2014-03-01 22:25:02 +0400

    changed code for schedule runs

--- worker.py
+++ worker.py
@@ -3,9 +3,28 @@
 __author__ = 'erik'
 
 from urllib2 import urlopen
+import time
+from datetime import datetime
 
 from pymongo import MongoClient
+import schedule
 
+
+def do_job():
+    print(str(datetime.now()) + ' Job started')
+    dao.insert_html(get_page_as_string(url1), url1, 1)
+    dao.insert_html(get_page_as_string(url2), url2, 2)
+    dao.insert_html(get_page_as_string(url3), url3, 3)
+    comparator.compare(1)
+    comparator.compare(2)
+    comparator.compare(3)
+    comparator.check(1)
+    comparator.check(2)
+    comparator.check(3)
+    print('Job ended')
+
+
+schedule.every(1).hours.do(do_job)
 
 connection_string = "mongodb://localhost"
 connection = MongoClient(connection_string)
@@ -29,17 +48,9 @@ dao = HtmlDAO(database)
 comparator = Comparator(database)
 
 if __name__ == '__main__':
-    dao.insert_html(get_page_as_string(url1), url1, 1)
-    dao.insert_html(get_page_as_string(url2), url2, 2)
-    dao.insert_html(get_page_as_string(url3), url3, 3)
-
-    comparator.compare(1)
-    comparator.compare(2)
-    comparator.compare(3)
-
-    comparator.check(1)
-    comparator.check(2)
-    comparator.check(3)
+    while True:
+        schedule.run_pending()
+        time.sleep(5)
 
 
 
