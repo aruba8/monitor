40ae3c2f048d24660ceff5b6a02e6ca261712f37
Erik Khalimov <biomaks@gmail.com>
2014-03-21 22:24:05 +0400

    moved config work into configparser file

---
+++ utils/configparser.py
@@ -0,0 +1,28 @@
+__author__ = 'erik'
+
+import os
+
+from ConfigParser import ConfigParser
+
+
+class Parser:
+    def __init__(self):
+        self.config = ConfigParser()
+        self.__cfile = open(os.path.join(os.getcwd(), 'config.ini'), 'r')
+        self.config.readfp(self.__cfile)
+        self.login = self.config.get('EmailConfigs', 'sender')
+        self.password = self.config.get('EmailConfigs', 'password')
+        self.to = self.config.get('EmailConfigs', 'to').split(',')
+
+    def get_login(self):
+        return self.login
+
+    def get_password(self):
+        return self.password
+
+    def get_to(self):
+        return self.to
+
+
+
+
--- workers/emailworker.py
+++ workers/emailworker.py
@@ -1,13 +1,12 @@
-from email.header import Header
-from email.mime.text import MIMEText
-
-import os
-
-
 __author__ = 'erik'
 
 import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+
 from utils.logerconf import Logger
+from utils.configparser import Parser
+
 
 logger = Logger()
 log = logger.get_logger()
@@ -15,28 +14,22 @@ log = logger.get_logger()
 
 class Emailer():
     def __init__(self):
-        from ConfigParser import ConfigParser
+        self.config = Parser()
 
-        self.config = ConfigParser()
-        self.__cfile = open(os.path.join(os.getcwd(), 'config.ini'), 'r')
-        self.config.readfp(self.__cfile)
-        self.login = self.config.get('EmailConfigs', 'sender')
-        self.password = self.config.get('EmailConfigs', 'password')
-        self.to = self.config.get('EmailConfigs', 'to').split(',')
 
     def init_msg(self, subject, text):
         message = MIMEText(text, _charset='utf-8')
         message['Subject'] = Header(subject, 'utf-8')
-        message['From'] = self.login
-        message['To'] = self.to.__str__()
+        message['From'] = self.config.get_login()
+        message['To'] = self.config.to.__str__()
         return message
 
     def send_email(self, message):
         try:
             smtp = smtplib.SMTP_SSL('smtp.yandex.ru', 465, timeout=10)
             smtp.set_debuglevel(0)
-            smtp.login(self.login, self.password)
-            smtp.sendmail(message['From'], self.to, message.as_string())
+            smtp.login(self.config.get_login(), self.config.get_password())
+            smtp.sendmail(message['From'], self.config.get_to(), message.as_string())
             smtp.close()
             log.info('Successfully sent email')
         except smtplib.SMTPException:
