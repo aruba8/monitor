e785394cdc70a78300f893398a661e1abf52c617
Erik Khalimov <biomaks@gmail.com>
2014-11-05 20:38:51 +0300

    fixed dependencies and config

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-MONGODB_SETTINGS = {'DB': 'diffs', 'USERNAME': '', 'PASSWORD': '', 'HOST': '127.0.0.1', 'PORT': 27017}
+MONGODB_SETTINGS = {'DB': 'diffs', 'USERNAME': '', 'PASSWORD': '', 'HOST': 'mongodb', 'PORT': 27017}
